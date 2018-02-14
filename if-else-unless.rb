898a8f1719cd3e5f084bc4cf3d7e9e87586258f0
Vijay M Shelkar <shelkarvijay94@gmail.com>
2018-02-14 17:06:58 +0530

    Add if-else program

---
+++ if-else-unless.rb
@@ -0,0 +1,32 @@
+# All loops Here
+
+#first if-else
+
+puts "Enter a number : "
+$x = gets.chomp.to_i
+if $x>5 
+	puts "X = #{$x}"
+elsif $x>10 && $x<20
+	puts " Your number is in between -10 to -20"
+	elsif $x == -30
+		puts "X = #{$x}"
+else
+	puts " Your number #{$x}"
+end
+
+#Another if-then-else example
+
+puts "Enter second number"
+$p = gets.chomp.to_i
+if $p >10 then puts " good"
+elsif $p<20
+	puts "p is greater than 0 and value of p is = #{$p}" 
+end
+
+#Unless modifier
+# Executes code if conditional is false. If the conditional is true, code specified in the else clause is executed.
+
+$var =  1
+print "1 -- Value is set\n" if $var
+$var = false
+print "unless executes when condition is false\n" unless $var
